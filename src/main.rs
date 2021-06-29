4cdfe8be718a778e481ea3fbf7845a13f1a1c1f9
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2021-06-29 14:19:56 -0700

    Add write_database

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,14 @@ fn main() {
     let key = arguments.next().unwrap();
     let value = arguments.next().unwrap();
     println!("Key: {}, value: {}", key, value);
+    match write_database(key, value) {
+        Ok(()) => {}
+        Err(err) => {
+            println!("Error: {}", err);
+        }
+    }
+}
+
+fn write_database(key: String, value: String) -> Result<(), std::io::Error> {
+    std::fs::write("kv.db", format!("{} {}\n", key, value))
 }
